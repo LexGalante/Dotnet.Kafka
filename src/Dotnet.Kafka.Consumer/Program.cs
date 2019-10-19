0cb6cadcfd15e0b05b2f75fcdfd9f4f38b632b82
Lex Galante <alex_gm_100@hotmail.com>
2019-10-19 13:37:36 -0300

    *

---
+++ src/Dotnet.Kafka.Consumer/Program.cs
@@ -0,0 +1,28 @@
+﻿using Confluent.Kafka;
+using System;
+
+namespace Dotnet.Kafka.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o nome da fila:");
+            var queue = Console.ReadLine();
+            using var consumer = GetConsumer(queue);
+            consumer.Subscribe(new string[] { queue });
+            var result = consumer.Consume();
+            if (result.IsPartitionEOF)
+                Console.WriteLine($"Mensagem: {result.Value}");
+        }
+
+        private static IConsumer<Ignore, string> GetConsumer(string queue) => new ConsumerBuilder<Ignore, string>(new ConsumerConfig
+        {
+            BootstrapServers = "localhost:8080",
+            GroupId = queue,
+            EnableAutoCommit = false,
+        })
+            .SetErrorHandler((_, e) => Console.WriteLine($"Ocorreu um erro inesperado: {e.Reason}"))
+            .Build();  
+    }
+}
---
+++ src/Dotnet.Kafka.Producer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Confluent.Kafka;
+
+namespace Dotnet.Kafka.Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Constroi um producer
+            using var producer = GetProducer();
+            //Obtem dados da fila e mensagem que deseja enviar
+            Console.WriteLine("Informe o nome da fila:");
+            var queue = Console.ReadLine();
+            Console.WriteLine("Informe a mensagem que deseja enviar:");
+            var message = Console.ReadLine();
+            //Envia mensagem
+            producer.Produce(queue, new Message<Null, string>()
+            {
+                Value = message                
+            });
+            //Retorna ok
+            Console.WriteLine($"Mensagem ({message}) enviada com sucesso!");
+            Console.ReadKey();
+            producer.Flush();
+        }
+
+        private static IProducer<Null, string> GetProducer() => new ProducerBuilder<Null, string>(new ProducerConfig()
+        {
+            BootstrapServers = "localhost:8080",
+            EnableSslCertificateVerification = false,
+            LogConnectionClose = true,
+            LogQueue = true,
+        }).Build();
+    }
+}
